91053dd3b39d1e3152ebf7cecb2d7f06ec34aebe
Natalia Zolotova <natakart@inbox.ru>
2025-11-07 12:29:04 +0300

    add all files

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
+    """Функция маскировки номера банковской карты"""
+    card_number = str(card_number)
+    mask_card_number = card_number[0:4] + " " + card_number[5:7] + "** **** " + card_number[-4:]
+    return mask_card_number
+
+
+def get_mask_account(account_number: Union[str, int]) -> str:
+    """Функция маскировки номера банковского счета"""
+    account_number = str(account_number)
+    mask_account = "**" + account_number[-4:]
+    return mask_account
