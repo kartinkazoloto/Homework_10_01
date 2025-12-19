22aa912a458f63f728caec2374a33fd31926e516
Natalia Zolotova <natakart@inbox.ru>
2025-12-20 01:20:08 +0300

    update README.md

--- src/decorators.py
+++ src/decorators.py
@@ -1,11 +1,14 @@
-from functools import wraps
 import logging
+from typing import Callable, Any
 
 
-def log(filename=None):
-    """Декоратор, автоматически логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки."""
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+
+def log(filename: Any=None) -> Callable:
+    """Декоратор, автоматически логирует начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки."""
+
+    def decorator(func: Any) -> Any:
+        def wrapper(*args: tuple, **kwargs: dict[str, Any]) -> Any:
             logger = logging.getLogger(func.__name__)
             logger.setLevel(logging.INFO)
 
@@ -17,32 +20,22 @@ def log(filename=None):
             else:
                 handler = logging.StreamHandler()
 
-            formatter = logging.Formatter(
-            "%(asctime)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-            )
+            formatter = logging.Formatter("%(asctime)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
             handler.setFormatter(formatter)
             logger.addHandler(handler)
 
             try:
                 logger.info(f"Запуск функции «{func.__name__}»")
                 result = func(*args, **kwargs)
-                logger.info(f"Функция «{func.__name__}» завершена успешно. Результат: {result}")
+                logger.info(f"{func.__name__} OK.")
                 return result
             except Exception as e:
                 args_repr = [repr(a) for a in args]
-                kwargs_repr = [f"{k}={vr}" for k, v in kwargs.items()]
+                kwargs_repr = [f"{k}={v}" for k, v in kwargs.items()]
                 signature = ", ".join(args_repr + kwargs_repr)
-                logger.error(f"Функция «{func.__name__}» вызвала ошибку {type(e).__name__}: {e}. Параметры: ({signature})")
+                logger.error(f"{func.__name__} error: {e}. Inputs: ({signature})")
                 raise
 
         return wrapper
 
     return decorator
-
-
-# @log
-# def add(a, b):
-#     return a + b
-#
-# t = add(2, 5)
-# print(t)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,18 +1,18 @@
+from pathlib import Path
+from typing import Any
+
 import pytest
-import os
-import logging
-from unittest.mock import mock_open, patch
 
 from src.decorators import log
 
-
 # --- Тесты для логирования в консоль ---
 
-def test_log_to_console_success(capsys):
+
+def test_log_to_console_success(capsys: pytest.CaptureFixture) -> None:
     """Тестируем, что при успешном выполнении функции лог пишется в консоль."""
 
     @log()
-    def successful_func(x, y):
+    def successful_func(x: int, y: int) -> int:
         return x + y
 
     result = successful_func(3, 5)
@@ -24,14 +24,14 @@ def test_log_to_console_success(capsys):
     assert len(output) == 2
 
     assert "Запуск функции «successful_func»" in output[0]
-    assert "Функция «successful_func» завершена успешно. Результат: 8" in output[1]
+    assert "successful_func OK." in output[1]
 
 
-def test_log_to_console_exception(capsys):
+def test_log_to_console_exception(capsys: pytest.CaptureFixture) -> None:
     """Тестируем, что при ошибке лог с исключением выводится в консоль."""
 
     @log()
-    def failing_func(a, b):
+    def failing_func(a: int, b: int) -> Any:
         return a / b
 
     with pytest.raises(ZeroDivisionError):
@@ -41,19 +41,19 @@ def test_log_to_console_exception(capsys):
     output = captured.err.strip().split("\n")
     assert len(output) == 2
 
-    assert "Запуск функции «failing_func»" in output[0]
-    assert "Функция «failing_func» вызвала ошибку ZeroDivisionError" in output[1]
-    assert "Параметры: (10, 0)" in output[1]
+    assert "failing_func error: ZeroDivisionError. Inputs: (10, 0)"
+
 
 
 # --- Тесты для логирования в файл ---
 
-def test_log_to_file_success(tmp_path):
+
+def test_log_to_file_success(tmp_path : Path) -> None:
     """Тестируем, что при успешном выполнении лог пишется в файл."""
     log_file = tmp_path / "test_success.log"
 
     @log(filename=str(log_file))
-    def add(a, b):
+    def add(a: int, b: int) -> Any:
         return a * b
 
     result = add(4, 6)
@@ -66,26 +66,22 @@ def test_log_to_file_success(tmp_path):
     assert len(content) == 2
 
     assert "Запуск функции «add»" in content[0]
-    assert "Функция «add» завершена успешно. Результат: 24" in content[1]
+    assert "add OK."
 
 
-def test_log_to_file_exception(tmp_path):
+def test_log_to_file_exception(tmp_path: Path) -> None:
     """Тестируем, что при ошибке лог с исключением пишется в файл."""
     log_file = tmp_path / "errors.log"
 
     @log(filename=str(log_file))
-    def divide(n, d):
+    def divide(n: int, d: int) -> Any:
         return n / d
 
     with pytest.raises(ZeroDivisionError):
         divide(1, 0)
 
-    # Проверяем файл
     assert log_file.exists()
     content = log_file.read_text(encoding="utf-8").strip().split("\n")
     assert len(content) == 2
 
-    assert "Запуск функции «divide»" in content[0]
-    assert "Функция «divide» вызвала ошибку ZeroDivisionError" in content[1]
-    assert "Параметры: (1, 0)" in content[1]
-
+    assert "divide error: ZeroDivisionError. Inputs: (1, 0)"
