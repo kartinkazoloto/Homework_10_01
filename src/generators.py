1de5d29edcade40c46835202433dc2002af77d7f
Natalia Zolotova <natakart@inbox.ru>
2025-12-19 22:45:11 +0300

    add test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,16 +1,19 @@
-def filter_by_currency(transactions, currency_code):
+from typing import Generator
+
+
+def filter_by_currency(transactions: list, currency_code: str) -> Generator[dict, None, None]:
     """Функция, которая принимает на вход список словарей, представляющих транзакции,
     а возвращает итератор, выдающий транзакции с указанной валютой."""
     return (t for t in transactions if t["operationAmount"]["currency"]["code"] == currency_code)
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list) -> Generator[str, None, None]:
     """Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
     for transaction in transactions:
         yield transaction.get("description", "")
 
 
-def card_number_generator(start, end):
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX,
     где X — цифра номера карты.
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
